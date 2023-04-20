6f02eb78c3bd5e4d41c62456f623727b336fdaee
karengomezs <karengomezsoto1@gmail.com>
2023-04-19 21:54:08 -0500

    design working pc

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -24,7 +24,10 @@ export default function Detail(props: Props) {
   const arrTags = props.response?.strTags?.split(",");
   const tags = arrTags?.map((tag, i) => {
     return (
-      <li key={tag}>
+      <li
+        key={tag}
+        className="px-2 bg-gray-200 rounded-xl flex items-baseline "
+      >
         <small>{tag}</small>
       </li>
     );
@@ -33,17 +36,39 @@ export default function Detail(props: Props) {
   return (
     <>
       <Navbar />
-      <div>
-        <img src={props.response?.strMealThumb} alt="" />
-        <time>20-30</time>
-        <h1>{props.response?.strMeal}</h1>
-        <p>estrellas</p>
-        <p>corazon</p>
-        <span>32 reviews</span>
-        <ul>{tags}</ul>
-      </div>
+      <main className="flex min-h-screen flex-col px-20 max-w-6xl mx-auto my-10">
+        <img
+          className="h-60 w-full object-cover rounded-xl mb-6"
+          src={props.response?.strMealThumb}
+        />
+        <div className="mb-6">
+          <time className="text-gray-700">
+            <i className="fa-regular fa-clock mr-2"></i>20-30
+          </time>
+          <div className="flex justify-between">
+            <div className="flex items-center gap-4">
+              <h1 className="text-red-700 text-3xl ">
+                {props.response?.strMeal}
+              </h1>
+              <div className="text-orange-600">
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+              </div>
+            </div>
+            <i className="fa-sharp fa-solid fa-heart text-3xl text-red-600 mr-10"></i>
+          </div>
 
-      <p>{props.response?.strInstructions}</p>
+          <span>32 Reviews</span>
+          <ul className="flex mt-5 gap-2">{tags}</ul>
+        </div>
+
+        <p className="max-w-[80%] mx-auto text-red-700">
+          {props.response?.strInstructions}
+        </p>
+      </main>
     </>
   );
 }
